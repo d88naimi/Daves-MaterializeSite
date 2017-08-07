bb0052fddaf571fdf79c4494f7b9d2c7394dfc39
David Naimi <d88naimi@gmail.com>
2017-08-07 10:17:26 -0700

    First commit for Dave's Materialize Site

---
+++ assets/js/creative.js
@@ -0,0 +1,22 @@
+
+
+(function($) {
+    "use strict"; // Start of use strict
+
+
+
+
+$(document).ready(function(){ $(".button-collapse").sideNav(); });
+
+
+$(document).ready(function(){ $(".dropdown-button").dropdown(); });
+
+
+   $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+        
+
+
+
+})(jQuery); // End of use strict
